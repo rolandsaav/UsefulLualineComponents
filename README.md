2a0bf81e35d6127d432100f7aef6b9937f8fda21
Roland Saavedra <rolandsaavedra22@gmail.com>
2025-06-10 23:49:58 -0700

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # UsefulLualineComponents
 Useful Lualine Components I use
+
+Right now there are two LuaLine Components in this plugin:
+
+localMarks: 
+This components tells you what local marks are set. 
+You can configure what the prefix is, what marks it tracks, and what the delimiter is.
+
+globalMarks:
+This components tells you what global marks are set. 
+You can configure what the prefix is, what marks it tracks, and what the delimiter is.
+
+
+More documentation coming... maybe
---
+++ lua/saav-useful.lua
@@ -0,0 +1,78 @@
+local M = {}
+local markNames = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+local localMarkNames = markNames:lower()
+
+function M.localMarks(config)
+	local prefix = "Lm:"
+	if config["prefix"] then
+		prefix = config["prefix"]
+	end
+
+	local marks = localMarkNames
+	if config["letters"] then
+		marks = config["letters"]
+	end
+
+	local delimiter = " "
+	if config["delimiter"] then
+		delimiter = config["delimiter"]
+	end
+
+	return function()
+		local status = prefix
+
+		local curBuf = vim.api.nvim_get_current_buf()
+
+		for i = 1, marks:len() do
+			local mark = marks:sub(i, i)
+			local location = vim.api.nvim_buf_get_mark(curBuf, mark)
+			if location[1] == 0 and location[2] == 0 then
+				goto continue
+			end
+
+			status = status .. delimiter .. mark
+
+			::continue::
+		end
+
+		return status
+	end
+end
+
+function M.globalMarks(config)
+	local prefix = "GM:"
+	if config["prefix"] then
+		prefix = config["prefix"]
+	end
+
+	local marks = markNames
+	if config["letters"] then
+		marks = config["letters"]:upper()
+	end
+
+	local delimiter = " "
+	if config["delimiter"] then
+		delimiter = config["delimiter"]
+	end
+
+	return function()
+		local status = prefix
+
+		for i = 1, marks:len() do
+			local mark = marks:sub(i, i)
+			local location = vim.api.nvim_get_mark(mark, {})
+			local line, col, bufnr, file = unpack(location)
+			if line == 0 and col == 0 and bufnr == 0 and file == "" then
+				goto continue
+			end
+
+			status = status .. delimiter .. mark
+
+			::continue::
+		end
+
+		return status
+	end
+end
+
+return M
